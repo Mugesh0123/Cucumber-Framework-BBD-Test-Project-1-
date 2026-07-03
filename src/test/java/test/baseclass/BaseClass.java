b5d2873c79b89afaa0d416d245f15c50db885e73
dineshraja1097 <developer@example.com>
2026-07-03 09:48:42 +0530

    Fix: Reorganize imports in BaseClass for better code organization
    - Moved all java.* imports to the top
    - Organized third-party imports (io.github, org.openqa.selenium)
    - Follows Java import conventions
    - Improves code readability
    - BUILD SUCCESS - No compilation errors

--- src/test/java/test/baseclass/BaseClass.java
+++ src/test/java/test/baseclass/BaseClass.java
@@ -7,17 +7,16 @@ import java.io.InputStream;
 import java.time.Duration;
 import java.util.Properties;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.Select;
 import org.openqa.selenium.support.ui.WebDriverWait;
-
-import io.github.bonigarcia.wdm.WebDriverManager;
-import org.openqa.selenium.chrome.ChromeOptions;
 
 public class BaseClass {
 	public static WebDriver driver;
