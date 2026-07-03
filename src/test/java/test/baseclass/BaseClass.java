b4301b672a9274d082391a1d8f1cba1c6a2edbcb
dineshraja1097 <developer@example.com>
2026-07-03 09:21:38 +0530

    Fix: Remove shadowing JavascriptExecutor variable in BaseClass
    - Removed 'private static final String JavascriptExecutor = null;' that was shadowing the imported JavascriptExecutor class
    - This variable was unused and caused potential issues with casting to JavascriptExecutor
    - Build now compiles successfully without any issues

--- src/test/java/test/baseclass/BaseClass.java
+++ src/test/java/test/baseclass/BaseClass.java
@@ -22,7 +22,6 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.chrome.ChromeOptions;
 
 public class BaseClass {
-	private static final String JavascriptExecutor = null;
 	public static WebDriver driver;
 	public static WebDriverWait wait;
 	public static WebElement element;
